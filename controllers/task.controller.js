949e5b417e73921d6f80eb55c824acf9af0daee0
mic615 <mic615>
2018-08-12 16:01:29 -0500

    created base API

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -10,7 +10,7 @@ exports.task_create = function (req, res) {
           name: req.body.name,
           category: req.body.category,
           createdDate:req.body.createdDate,
-        startDate:req.body.startDate,
+          startDate:req.body.startDate,
           endDate:req.body.endDate,
           assigned:req.body.assigned,
           completed:req.body.completed
@@ -29,4 +29,53 @@ exports.task_details = function (req, res) {
         if (err) return next(err);
         res.send(task);
     })
+};
+exports.task_get_by_name = function (req, res) {
+    Task.find({name: req.params.name}, function (err, task) {
+        if (err) return next(err);
+        res.send(task);
+    })
+};
+
+exports.task_get_by_category = function (req, res) {
+    Task.find({category: req.params.category}, function (err, task) {
+        if (err) return next(err);
+        res.send(task);
+    })
+};
+
+exports.task_get_by_assigned = function (req, res) {
+    Task.find({assigned: req.params.assigned}, function (err, task) {
+        if (err) return next(err);
+        res.send(task);
+    })
+};
+
+exports.task_get_by_completed = function (req, res) {
+    Task.find({completed: req.params.completed}, function (err, task) {
+        if (err) return next(err);
+        res.send(task);
+    })
+};
+
+exports.task_update = function (req, res) {
+    Task.findByIdAndUpdate(req.params.id, { $set: { name: req.body.name, category: req.body.category }},
+    function (err, task) {
+        if (err) return next(err);
+        res.send('task updated');
+    });
+};
+exports.task_update_assigned = function (req, res) {
+    Task.findByIdAndUpdate(req.params.id, { $set: { assigned: req.body.assigned, startDate: req.body.startDate }},
+    function (err, task) {
+        if (err) return next(err);
+        res.send('assigned updated');
+    });
+};
+exports.task_update_completed = function (req, res) {
+    Task.findByIdAndUpdate(req.params.id, { $set: { completed: req.body.completed , startDate: req.body.endDate }},
+    function (err, task) {
+        if (err) return next(err);
+        res.send('completed updated');
+    });
 };
